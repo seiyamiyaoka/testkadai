c114d1e6dfa352c19323b8c4b8c1d398a03e6714
shohei_nakamura <jose920nkmr@yahoo.com>
2017-09-04 10:32:14 +0000

    init

---
+++ app/models/blog.rb
@@ -0,0 +1,6 @@
+class Blog < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :content, length: { minimum: 1 }
+    validates :content, length:{maximum: 140}
+end
